93955b1d12076c661d5c73188b6bdff347d63d2a
Jon Kragskow <jonkragskow@gmail.com>
2023-02-17 11:21:07 +0000

    docs: add license

--- pages/core/utils.py
+++ pages/core/utils.py
@@ -1,3 +1,20 @@
+"""
+                    Waveplot: An online wavefunction viewer
+                    Copyright (C) 2023  Jon G. C. Kragskow
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import plotly.colors as pc
 import dash_bootstrap_components as dbc
 from dash import html, dcc, callback, page_registry
