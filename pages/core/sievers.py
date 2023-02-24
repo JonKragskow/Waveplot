9e89580cdfb5684e37a178484295d6ccce81f77b
Jon Kragskow <jonkragskow@gmail.com>
2023-02-24 00:17:43 +0000

    perf: angular range

--- pages/core/sievers.py
+++ pages/core/sievers.py
@@ -59,7 +59,7 @@ def compute_trisurf(a_2, a_4, a_6, scale=2):
 
     # Create angular grid, with values of theta
     phi = np.linspace(0, np.pi, 30)
-    theta = np.linspace(0, np.pi*2, 100)
+    theta = np.linspace(0, np.pi*2, 70)
     u, v = np.meshgrid(phi, theta)
     u = u.flatten()
     v = v.flatten()
