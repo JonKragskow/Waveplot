aebc650d824f99471dcc126e5c56ac630e74b055
jonkragskow <jonkragskow@gmail.com>
2023-12-18 13:29:28 +0000

    feat: add script to create orbital files

---
+++ make_orbs.py
@@ -0,0 +1,38 @@
+from pages.core.orbitals import calc_wav
+
+orbs = [
+    '1s',
+    '2s',
+    '3s',
+    '4s',
+    '5s',
+    '6s',
+    '2p',
+    '3p',
+    '4p',
+    '5p',
+    '6p',
+    '3dz2',
+    '4dz2',
+    '5dz2',
+    '6dz2',
+    '3dxy',
+    '4dxy',
+    '5dxy',
+    '6dxy',
+    '4fz3',
+    '5fz3',
+    '6fz3',
+    '4fxyz',
+    '5fxyz',
+    '6fxyz',
+    '4fyz2',
+    '5fyz2',
+    '6fyz2',
+    'sp',
+    'sp2',
+    'sp3'
+]
+
+for orb in orbs:
+    calc_wav(orb)
